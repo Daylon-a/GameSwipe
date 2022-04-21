6a04239d5dfb9ee38684c1059ca80d1b4b240bd0
Daylon-a <adkisondkad@gmail.com>
2022-04-20 21:45:37 -0500

    New commit

--- gamecarousel/views.py
+++ gamecarousel/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import GameData
+from django.contrib.auth import authenticate
 
 # Create your views here.
 
@@ -11,32 +12,42 @@ def home(request):
 def mainpage(request):
 
 
-    #for in GameData.objects.all():
-       #selectedgame = GameData.objects.filter(genre='rpg').order_by('?')[0:1]
-        #gname = getattr(selectedgame, 'gname', 'Not Found')
-        #gdescription = getattr(selectedgame, 'description', 'No Description Found')
-
-    #selectedgame = GameData.objects.order_by('?')[0:1].get()
+#selectedgame = GameData.objects.filter(genre='rpg').order_by('?')[0:1]
     #gname = getattr(selectedgame, 'gname', 'Not Found')
-    #gdescription = getattr(selectedgame, 'description', 'No Description Found')
+    #description = getattr(selectedgame, 'description', 'No Description Found')
+
+    selectedgame = GameData.objects.order_by('?')[0:1].get()
+    gname = getattr(selectedgame, 'gname', 'Not Found')
+    gdescription = getattr(selectedgame, 'description', 'No Description Found')
     
-    #secondselectedgame = GameData.objects.order_by('?')[1:2].get()
-    #secondgname = getattr(secondselectedgame, 'gname', 'Not Found')
-    #secondgdescription = getattr(secondselectedgame, 'description', 'No Description Found')
+    secondselectedgame = GameData.objects.order_by('?')[1:2].get()
+    secondgname = getattr(secondselectedgame, 'gname', 'Not Found')
+    secondgdescription = getattr(secondselectedgame, 'description', 'No Description Found')
 
-    #thirdselectedgame = GameData.objects.order_by('?')[2:3].get()
-    #thirdgname = getattr(thirdselectedgame, 'gname', 'Not Found')
-    #thirdgdescription = getattr(thirdselectedgame, 'description', 'No Description Found')
+    thirdselectedgame = GameData.objects.order_by('?')[2:3].get()
+    thirdgname = getattr(thirdselectedgame, 'gname', 'Not Found')
+    thirdgdescription = getattr(thirdselectedgame, 'description', 'No Description Found')
 
-    #fourthselectedgame = GameData.objects.order_by('?')[3:4].get()
-    #fourthgname = getattr(fourthselectedgame, 'gname', 'Not Found')
-    #fourthgdescription = getattr(fourthselectedgame, 'description', 'No Description Found')
+    fourthselectedgame = GameData.objects.order_by('?')[3:4].get()
+    fourthgname = getattr(fourthselectedgame, 'gname', 'Not Found')
+    fourthgdescription = getattr(fourthselectedgame, 'description', 'No Description Found')
 
     #gamequeryset = GameData.objects.all()
-    selectedgame = GameData.objects.filter(genre='RPG').order_by('?')
+    #selectedgame = GameData.objects.filter(genre='RPG').order_by('?')
 
     
 
     return render(request, 'mainpage.html', 
     {'selectedgame': selectedgame, 
+    'gname': gname,
+    'gdescription': gdescription,
+    'secondgname': secondgname,
+    'secondgdescription': secondgdescription,
+    'thirdgname': thirdgname,
+    'thirdgdescription': thirdgdescription,
+    'fourthgname':  fourthgname,
+    'fourthgdescription': fourthgdescription,
+    'secondselectedgame': secondselectedgame,
+    'thirdselectedgame': thirdselectedgame,
+    'fourthselectedgame': fourthselectedgame,
     })
